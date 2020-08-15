9be2265abc3a08f77e4d29bc8215436b14af92cf
jfaz5 <fake-email@fake-email.com>
2020-08-14 22:18:06 -0400

    fourth commit

--- Client/src/main/java/us/group/client/ui/gui/GraphicalUserInterface.java
+++ Client/src/main/java/us/group/client/ui/gui/GraphicalUserInterface.java
@@ -1,7 +1,7 @@
 package us.group.client.ui.gui;
 
-import us.group.client.types.UserInterfaceType;
 import us.group.client.ui.UserInterface;
+import us.group.client.types.UserInterfaceType;
 
 public class GraphicalUserInterface extends UserInterface {
     public UserInterfaceType getUIType() {
