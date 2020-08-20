d231c995e5fd785d5226ff35fa0651149617eb4e
jfaz5 <fake-email@fake-email.com>
2020-08-20 12:07:43 -0400

    gui created

--- Client/src/main/java/us/group/client/ui/gui/GraphicalUserInterface.java
+++ Client/src/main/java/us/group/client/ui/gui/GraphicalUserInterface.java
@@ -1,10 +1,22 @@
 package us.group.client.ui.gui;
 
+import javafx.stage.Stage;
 import us.group.client.ui.UserInterface;
 import us.group.client.types.UserInterfaceType;
 
 public class GraphicalUserInterface extends UserInterface {
+    private GraphicalUserInterfaceHelper guiHelper;
+
+    public GraphicalUserInterface() {
+        this.guiHelper = new GraphicalUserInterfaceHelper();
+    }
+
     public UserInterfaceType getUIType() {
         return UserInterfaceType.GUI;
     }
+
+    public void start(Stage primaryStage) {
+        this.guiHelper.start(primaryStage);
+    }
+
 }
---
+++ Client/src/main/java/us/group/client/ui/gui/GraphicalUserInterfaceHelper.java
@@ -0,0 +1,13 @@
+package us.group.client.ui.gui;
+
+import javafx.stage.Stage;
+import javafx.application.Application;
+
+public class GraphicalUserInterfaceHelper extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("GroupUs");
+
+        primaryStage.show();
+    }
+}
