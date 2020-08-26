1c420dc2b722bf3bb1a465b18c4fcf43597adec6
jfaz5 <fake-email@fake-email.com>
2020-08-26 00:47:06 -0400

    gui update

--- Client/src/main/java/us/group/client/ui/gui/GraphicalUserInterfaceHelper.java
+++ Client/src/main/java/us/group/client/ui/gui/GraphicalUserInterfaceHelper.java
@@ -1,6 +1,10 @@
 package us.group.client.ui.gui;
 
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 import javafx.scene.Group;
 import javafx.stage.Screen;
@@ -10,15 +14,12 @@ import javafx.scene.layout.HBox;
 import javafx.scene.image.Image;
 import javafx.geometry.Rectangle2D;
 import javafx.geometry.Orientation;
-import javafx.scene.control.Button;
-import javafx.scene.control.Tooltip;
 import javafx.scene.image.ImageView;
-import javafx.scene.control.Separator;
 import javafx.application.Application;
 import javafx.collections.ObservableList;
 
 public class GraphicalUserInterfaceHelper extends Application {
-    
+
     /**
      * Creates a button to be displayed on the left sidebar.
      *
@@ -66,26 +67,38 @@ public class GraphicalUserInterfaceHelper extends Application {
     public void start(Stage primaryStage) {
         // Vertical boxes for left side of screen
         VBox firstVerticalBox = new VBox(10);
-        VBox secondVerticalBox = new VBox(10);
+        VBox secondVerticalBox = new VBox(10); // Filler
+        VBox thirdVerticalBox = new VBox(10);
+        VBox fourthVerticalBox = new VBox(10); // Filler
 
-        // Separators for left side of screen
+        // Line separators
         Separator firstSeparator = new Separator(Orientation.HORIZONTAL);
         Separator secondSeparator = new Separator(Orientation.VERTICAL);
+        Separator thirdSeparator = new Separator(Orientation.VERTICAL);
+        Separator fourthSeparator = new Separator(Orientation.HORIZONTAL);
+
+        // Set line heights
         int windowHeight = getWindowHeight();
         secondSeparator.setStyle("-fx-height: " + windowHeight + "px; -fx-min-height: " + windowHeight + "px;");
+        thirdSeparator.setStyle("-fx-height: " + windowHeight + "px; -fx-min-height: " + windowHeight + "px;");
 
         // Object for loading resources
         ClassLoader classLoader = getClass().getClassLoader();
 
-        // Create home button for left side of screen
+        // Create home button for left sidebar
         InputStream homeIconStream = classLoader.getResourceAsStream("img/home_icon.png");
         Image homeIconImage = new Image(homeIconStream);
         Button homeButton = createLeftSidebarButton(homeIconImage, "Home");
 
-        // Create add server button for left side of screen
+        // Create add server button for left sidebar
         InputStream plusIconStream = classLoader.getResourceAsStream("img/plus_icon.png");
         Image plusIconImage = new Image(plusIconStream);
         Button addServerButton = createLeftSidebarButton(plusIconImage, "Add a Server");
+
+        // Create explore button for left sidebar
+        InputStream magnifyingGlassIconStream = classLoader.getResourceAsStream("img/magnifying_glass_icon.png");
+        Image magnifyingGlassIconImage = new Image(magnifyingGlassIconStream);
+        Button exploreButton = createLeftSidebarButton(magnifyingGlassIconImage, "Explore Public Servers");
 
         // Add components to left sidebar
         ObservableList firstVerticalBoxList = firstVerticalBox.getChildren();
@@ -93,6 +106,34 @@ public class GraphicalUserInterfaceHelper extends Application {
         firstVerticalBoxList.add(homeButton);
         firstVerticalBoxList.add(firstSeparator);
         firstVerticalBoxList.add(addServerButton);
+        firstVerticalBoxList.add(exploreButton);
+
+        // Create conversation text field
+        TextField conversationTextField = new TextField();
+        conversationTextField.setMinWidth(200);
+        conversationTextField.setPromptText("Find or start a conversation");
+
+        // Create friends button
+        InputStream waveIconStream = classLoader.getResourceAsStream("img/wave_icon.png");
+        Image waveIconImage = new Image(waveIconStream);
+        ImageView waveIconView = new ImageView(waveIconImage);
+        waveIconView.setFitWidth(35);
+        waveIconView.setFitHeight(35);
+        Button friendsButton = new Button();
+        friendsButton.setGraphic(waveIconView);
+        friendsButton.setText("Friends");
+        friendsButton.setMinWidth(200);
+        friendsButton.setMinHeight(35);
+        friendsButton.setAlignment(Pos.BASELINE_LEFT);
+        friendsButton.setFont(Font.font(Font.getDefault().getName(), FontWeight.BOLD, 15.0));
+        friendsButton.setStyle("-fx-border-color: black; -fx-border-width: 2px;");
+
+        // Add components to second left sidebar
+        ObservableList thirdVerticalBoxList = thirdVerticalBox.getChildren();
+        thirdVerticalBoxList.add(fourthVerticalBox);
+        thirdVerticalBoxList.add(conversationTextField);
+        thirdVerticalBoxList.add(fourthSeparator);
+        thirdVerticalBoxList.add(friendsButton);
 
         // Horizontal boxes for left side of screen
         HBox firstHorizontalBox = new HBox(10);
@@ -103,6 +144,8 @@ public class GraphicalUserInterfaceHelper extends Application {
         firstHorizontalBoxList.add(secondHorizontalBox);
         firstHorizontalBoxList.add(firstVerticalBox);
         firstHorizontalBoxList.add(secondSeparator);
+        firstHorizontalBoxList.add(thirdVerticalBox);
+        firstHorizontalBoxList.add(thirdSeparator);
 
         // Add horizontal layout to group
         Group root = new Group();
